36b38aa5bdd90d6496d76b00650716cd3239b6ba
iamjunhyeong <dddodo447@gmail.com>
2025-04-19 04:53:45 +0900

    Feat: Docker 설정정

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "✅ Starting Flask Camera App..."
+
+# .env 로드
+source .env
+
+# venv 없이 직접 실행
+python run.py
---
+++ venv_start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /home/admin/workspace/Server
+source venv/bin/activate
+python run.py
